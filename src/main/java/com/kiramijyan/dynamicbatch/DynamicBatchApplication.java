d7984b9e815ba293001223789c73743448af9f55
Hovhannes Kiramijyan <hkiramijyan@gmail.com>
2023-09-18 21:17:54 +0200

    Initial commit

---
+++ src/main/java/com/kiramijyan/dynamicbatch/DynamicBatchApplication.java
@@ -0,0 +1,13 @@
+package com.kiramijyan.dynamicbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiramijyan/dynamicbatch/DynamicBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiramijyan.dynamicbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
